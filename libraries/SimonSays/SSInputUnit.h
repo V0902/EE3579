5ab560e54eac2c6725037c695f93082f9539c4de
prucinski <p.rucinski.19@abdn.ac.uk>
2022-02-16 20:27:10 +0000

    Input

--- libraries/SimonSays/SSInputUnit.h
+++ libraries/SimonSays/SSInputUnit.h
@@ -3,3 +3,54 @@
 #else
 #include <Arduino.h>
 #endif
+
+#include <Basic_Input.h>
+
+class inputUnit(){
+protected:
+	//array of binary inputs, for each button.
+	
+	bool binaryInputs[5] = {false, false, false, false, false};
+	in_digital digitalPins[5];
+	int size;
+public:
+	inputUnit(int givenSize){
+		size = givenSize;
+	}
+	void setDigitalPins(int[5] pins){
+		for(int i = 0; i < size; i++){
+		//initializaiton of the in_digital array.
+			digitalPins[i] = new in_digital(pins[i]);
+		}
+	}
+	void readBinaryInputs(){
+		for(int i = 0; i < size; i++){
+			//detection variable. Not necessary for implementation
+			int detection;
+			digitalPins[i].read_input(detection);
+			if(detection == LOW){
+				Serial.println("Button pressed at index "+ i);
+				buttonInputs[i] = true;
+
+			}
+
+		}
+	}
+	void resetButtons(){
+		for(int i = 0; i < size; i++){
+			binaryInputs[i] = false;
+		}
+	}
+
+	int returnPressedButton(){
+		for(int i = 0; i < size; i++){
+			if(binaryInputs[i]){
+			//for now, assuming that the user will click one button at a time.
+				return i;
+			}
+		}
+		resetButtons();
+	}
+}
+
+#endif
