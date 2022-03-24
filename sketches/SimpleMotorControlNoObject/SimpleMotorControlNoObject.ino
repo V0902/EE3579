17cf4a8ad2022c4e7088ce972b077f29a6ec0e9d
Rucinski <u01pr19@abdn.ac.uk>
2022-03-24 13:55:43 +0000

    some basic work done

--- sketches/SimpleMotorControlNoObject/SimpleMotorControlNoObject.ino
+++ sketches/SimpleMotorControlNoObject/SimpleMotorControlNoObject.ino
@@ -9,10 +9,15 @@ IntervalCheckTimer speed_check;
 //FV
 int target_index, consecutive_count;
 //
+bool switched;
 
 void setup()
 {
-  
+  //set enable pin
+  pinMode(13, OUTPUT);
+  //set direction pin!
+  pinMode(4, OUTPUT);
+  digitalWrite(13, LOW);
   // enable the interrupt (int_0 works via pin2)
   // enable the interrupt (int_1 works via pin3)
   rotation_counter.setupSpeedMeasure(int_1);
@@ -31,6 +36,19 @@ void setup()
 
 void loop()
 {
+    Serial.println("Turned off");
+    digitalWrite(13, LOW);
+    delay(5000);
+    digitalWrite(13, HIGH);
+    Serial.println("Turned on");
+    Serial.println("Oop, I'm switching direction now!");
+    digitalWrite(4, switched);
+    switched = !switched;
+    delay(5000);
+    digitalWrite(13, LOW);
+    Serial.println("Turned off again");
+    
+  
   
   if(speed_check.isMinChekTimeElapsedAndUpdate())
   {
